30d2b935c62cb5292b7b7dcae9f82cc09d03d292
AhmadRaza09 <ahmadrazamul09@gmail.com>
2024-08-22 10:37:21 +0500

    added the documentation for the queue interface methods

--- queue/src/main/java/org/queue/contract/Queue.java
+++ queue/src/main/java/org/queue/contract/Queue.java
@@ -1,16 +1,45 @@
 package org.queue.contract;
 
 public interface Queue {
+    /**
+     * This method check the queue is element contains in the queue or not
+     *
+     * @return boolean true if element contains otherwise return false
+     */
     public boolean isEmpty();
 
+    /**
+     * This method give tell how many elements in the queue
+     *
+     * @return int return the number of element in the queue
+     */
     public int size();
 
+    /**
+     * This method give the first element of the queue
+     *
+     * @return Object first element of the queue
+     */
     public Object getFirst();
 
     //    transformers
+
+    /**
+     * This method delete all the elements fromm the queue
+     */
     public void clear();
 
+    /**
+     * This method add the element in the queue. It adds the element always in the end
+     *
+     * @param elem the element that need to be added in the queue
+     */
     public void addLast(Object elem);
 
+    /**
+     * This method remove the element from the queue. It always from the element from the start of the queue
+     *
+     * @return Object return the element that is removed recently
+     */
     public Object removeFirst();
 }
